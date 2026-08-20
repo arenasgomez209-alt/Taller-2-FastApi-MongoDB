40070cd1a2fbef52d60a6767a87e2c9b4b95e50e
Matias Arenas <arenasgomez209@github.local>
2026-08-20 16:00:43 -0500

    Actualizar views.py con cambios actuales

--- frontend/app_frontend/views.py
+++ frontend/app_frontend/views.py
@@ -1,7 +1,7 @@
 import requests
 from django.shortcuts import render, redirect
 
-API_URL = "https://taller-2-fastapi-mongodb-1.onrender.com"
+API_URL = "[https://taller-2-fastapi-mongodb-1.onrender.com](https://taller-2-fastapi-mongodb-1.onrender.com)"
 
 
 def lista_productos(request):
